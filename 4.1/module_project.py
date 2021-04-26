e2d99ef52fe7bf8674f953293b633e5d53b054ef
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-26 17:01:47 -0500

    Added new file for CodeSignal assignment and added prompt for exercise 1

---
+++ 4.1/module_project.py
@@ -0,0 +1,25 @@
+
+""" Sprint 4.1: Hash Tables I - CodeSignal Assignment """
+
+
+
+"""
+Exercise 1: Find The Single Number (task 4 of 6)
+
+You are given a non-empty array of integers. One element appears exactly once, with every other element appearing 
+at least twice, perhaps more.
+
+Write a function that can find and return the element that appears exactly once.
+
+
+* Examples:
+    Input: [1,1,2,1]
+    Output: 2
+    Example 2:
+
+    Input: [1,2,1,2,1,2,80]
+    Output: 80
+
+
+* Note: You should be able to develop a solution that has O(n) time complexity.
+"""
