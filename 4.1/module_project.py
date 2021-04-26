7b19dbd46fe13b0a61d93b27d42e04e98c641eff
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-26 18:20:17 -0500

    Added solution for exercise 2

--- 4.1/module_project.py
+++ 4.1/module_project.py
@@ -63,7 +63,6 @@ print(csFindTheSingleNumber(nums))
 
 
 
-
 """
 Exercise 2: Average of Top Five (task 5 of 6)
 
@@ -86,4 +85,75 @@ averages should be calculated using integer division.
 
 * Notes:
     The score of the students is between 1 and 100.
+
+
+* UPER - Plan:
+    - init a new dict
+    - students id (scores[i][0]) as the key in dict
+    - since student id is unique, we can be sure that there won't be any collisions
+
+    - loop through input array
+        - check if student is in dict yet
+            - if so append score to students score array
+        - else
+            - create a new key-val pair for student in dict
+            - init a new array
+            - append score to array
+            - set dict[scores[i][0]] = new_array (with the score held inside)
+    
+    - loop through the dict and check len of each score array
+        - if scores count is greater than 5:
+            - sort scores
+            - get the sum of scores[-5]
+    - if the student has less than 5 scores in dict[stu_id], append the score to values
+    - otherwise compare new value to 5 values currently in dict for student
+        - if the new value is less than all 5, pass
+        - otherwise keep the new value and remove the min value
 """
+
+def csAverageOfTopFive(scores):
+    grade_book = dict()
+
+    for i in range(len(scores)):
+        stu_id = scores[i][0]
+        grade = scores[i][1]
+
+        if stu_id in grade_book:
+            grade_book[stu_id].append(grade)
+        else:
+            stu_grades = [grade]
+            grade_book[stu_id] = stu_grades
+
+
+    averages = []
+    
+    for student in grade_book.keys():
+        stu_grades = grade_book[student]
+
+        if len(stu_grades) > 5:
+            stu_grades.sort()
+            stu_grades = stu_grades[-5:]
+
+
+        grade_sum = sum(stu_grades)
+        avg = (grade_sum // len(stu_grades))
+
+        averages.append([student, avg])
+
+
+    return averages
+
+
+
+print("\nExercise 2:")
+scores = [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]] # expected: [[1,87],[2,88]]
+print(csAverageOfTopFive(scores))
+
+scores = []
+print(csAverageOfTopFive(scores)) # expected: []
+
+scores = [[1,2]]
+print(csAverageOfTopFive(scores)) # expected: [[1,2]]
+
+scores = [[1,96],[3,55],[2,66],[2,72],[1,82],[4,56],[1,59],[5,73],[2,93],[2,0],[2,76],[3,0]]
+print(csAverageOfTopFive(scores)) # expected: [[1, 79], [3, 27], [2, 61], [4, 56], [5, 73]]
