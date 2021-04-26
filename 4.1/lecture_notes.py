682a0704767db27e207473ad444a4a7f94efbaee
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-26 13:55:22 -0500

    Added file for lecture notes and intro to hash tables basics

---
+++ 4.1/lecture_notes.py
@@ -0,0 +1,22 @@
+
+
+""" Sprint 4.1 Lecture Notes - Hash Tables I """
+
+
+
+"""
+* Hash Table Basics:
+    - great for searches if we know the key
+    - one of most common data structures
+        - dictionaries and sets built on top of hash tables
+    
+
+    * hashing functions:
+        - take in a variable input
+            - typically a string
+        - deterministic
+            - same input always has same resulting output
+        - good hashing fxns have minimal duplication of output values
+            - want our output to be distributed across a wide range of values
+        - hashing should always be constant time to get value for key
+"""
