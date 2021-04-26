e2e6d86771a6285fbea998c6185a4f4f7966dc36
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-26 17:23:41 -0500

    Added solution for exercise 1

--- 4.1/module_project.py
+++ 4.1/module_project.py
@@ -15,7 +15,6 @@ Write a function that can find and return the element that appears exactly once.
 * Examples:
     Input: [1,1,2,1]
     Output: 2
-    Example 2:
 
     Input: [1,2,1,2,1,2,80]
     Output: 80
@@ -23,3 +22,40 @@ Write a function that can find and return the element that appears exactly once.
 
 * Note: You should be able to develop a solution that has O(n) time complexity.
 """
+
+# init a new 'seen nums' dict
+# loop through input
+# check if num is in the seen dict
+    # if it is, if the value is true, change to false
+    # if the fvalue is false, pass
+# if not add to seen nums dict and set to tru (true == only appears once)
+
+# use a second loop and return the key whose value is True
+
+def csFindTheSingleNumber(nums):
+    seen_nums = dict()
+
+    for num in nums:
+        if num in seen_nums.keys():
+            seen_nums[num] = False
+        else:
+            seen_nums[num] = True
+
+    
+    for key in seen_nums.keys():
+        if seen_nums[key] == True:
+            return key
+    
+    return None
+
+
+
+print("\nExercise 1:")
+nums = [2, 2, -3, 2]
+print(csFindTheSingleNumber(nums))
+
+nums = [1,1,9,1,2,3,4,4,5,3,4,9,6,5,2]
+print(csFindTheSingleNumber(nums))
+
+nums = [0, 1, 0, 1, 0, 1, 99]
+print(csFindTheSingleNumber(nums))
