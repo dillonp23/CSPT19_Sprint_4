db82116ce99f1bc638414a93e99f11fb32481554
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-27 15:35:51 -0500

    Added prompt for exercise 1

--- 4.2/module_project.py
+++ 4.2/module_project.py
@@ -1,2 +1,47 @@
 
 """ Sprint 4.2: Hash Tables II - CodeSignal Assignment """
+
+
+
+"""
+Exercise 1: Isomorphic Strings (task 3 of 5)
+
+Given two strings a and b, determine if they are isomorphic.
+Two strings are isomorphic if the characters in 'a' can be replaced to get 'b'.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character but a character may map to itself.
+
+
+* Examples:
+
+    1.  Input: 
+            a = "odd"
+            b = "egg"
+
+        Output: true
+
+
+    2.  Input:
+            a = "foo"
+            b = "bar"
+
+        Output: false
+
+
+    3.  Input:
+            a = "abca"
+            b = "zbxz"
+
+        Output: true
+
+
+    4.  Input:
+            a = "abc"
+            b = ""
+
+        Output: false
+"""
+
+def csIsomorphicStrings(a, b):
+    pass
