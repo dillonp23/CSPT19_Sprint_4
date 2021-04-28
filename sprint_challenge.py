e90d6a9caeb539653c807bc980a3276510c0d44c
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-27 20:48:42 -0500

    Added prompt for exercise 3

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -142,3 +142,51 @@ print(first_not_repeating_character("bcb")) # expected: c
 print(first_not_repeating_character("bcccccccb")) # expected: _
 print(first_not_repeating_character("abcdefghijklmnopqrstuvwxyziflskecznslkjfabe")) # expected: d
 print(first_not_repeating_character("bcccccccccccccyb")) # expected: y
+
+
+
+
+
+"""
+Exercise 3: Uncover Spy (task 3 of 3)
+
+In a city-state of n people, there is a rumor going around that one of the n people is a spy for the 
+neighboring city-state.
+
+The spy, if it exists:
+    - Does not trust anyone else.
+    - Is trusted by everyone else (he's good at his job).
+    - Works alone; there are no other spies in the city-state.
+    - You are given a list of pairs, trust. Each trust[i] = [a, b] represents the fact that person a trusts person b.
+
+If the spy exists and can be found, return their identifier. Otherwise, return -1.
+
+
+* Examples:
+
+    Input: n = 2, trust = [[1, 2]]
+    Output: 2
+    Explanation: Person 1 trusts Person 2, but Person 2 does not trust Person 1, so Person 2 is the spy.
+
+    Input: n = 3, trust = [[1, 3], [2, 3]]
+    Output: 3
+    Explanation: Person 1 trusts Person 3, and Person 2 trusts Person 3, but Person 3 does not trust either 
+    Person 1 or Person 2. Thus, Person 3 is the spy.
+
+    Input: n = 3, trust = [[1, 3], [2, 3], [3, 1]]
+    Output: -1
+    Explanation: Person 1 trusts Person 3, Person 2 trusts Person 3, and Person 3 trusts Person 1. Since everyone 
+    trusts at least one other person, there is no spy.
+
+    Input: n = 3, trust = [[1, 2], [2, 3]]
+    Output: -1
+    Explanation: Person 1 trusts Person 2, and Person 2 trusts Person 3. However, in this situation, we don't have 
+    any one person who is trusted by everyone else. So we can't determine who the spy is in this case.
+
+    Input: n = 4, trust = [[1, 3],[1, 4],[2, 3],[2, 4],[4, 3]]
+    Output: 3
+    Explanation: Person 1 trusts Person 3 and P
+"""
+
+def uncover_spy(n, trust):
+    pass
