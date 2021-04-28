b65dcf87ed88898d442e8c0da96aa4eadf307bc8
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-27 20:37:57 -0500

    Added prompt for exercise 2

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -81,3 +81,27 @@ f.next = g
 g.next = h
 
 print(condense_linked_list(a)) # expected: (3) -> (4) -> (2) -> (6) -> (1) -> N
+
+
+
+
+
+"""
+Exercise 2: First Non-Repeating Character (task 2 of 3)
+
+Given a string s consisting of small English letters, find and return the first instance of a non-repeating 
+character in it. If there is no such character, return '_'.
+
+
+* Examples:
+    For s = "abacabad", the output should be
+    first_not_repeating_character(s) = 'c'.
+
+    - There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+
+    For s = "abacabaabacaba", the output should be
+    first_not_repeating_character(s) = '_'.
+
+    - There are no characters in this string that do not repeat.
+"""
