e268089c3b7fda3a743e98361cfdbe01b831ab65
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-27 20:35:41 -0500

    Added solution for exercise 1

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -15,13 +15,69 @@ occurred in the linked list. Your function should return a reference to the head
     Input: (3) -> (4) -> (3) -> (2) -> (6) -> (1) -> (2) -> (6) -> N
     Output: (3) -> (4) -> (2) -> (6) -> (1) -> N
     Explanation: The input list contains redundant nodes (3), (6), and (2), so those should be removed from the list.
+
+
+* UPER - Plan:
+    * keywords: return ref to head, remove duplicate values
+
+    - start with a curr pointer to head of list
+    - prev pointer as None
+    - init an emtpy 'seen values' set
+    - since the head will never be removed we can safely return head at end
+
+    - iterate through list
+    - at each node check if curr_value in seen nums
+        - if it is we need to remove the node
+        - use a temp pointer to store next node in list
+        - set prev.next to temp
+    - if not in seen nums, add to set and continue
+    - update prev and curr to next nodes in list
 """
 
 class ListNode(object):
-  def __init__(self, x):
-    self.value = x
-    self.next = None
+    def __init__(self, x):
+        self.value = x
+        self.next = None
+
+    def __repr__(self):
+        return f"{self.value}->{self.next}"
 
 
 def condense_linked_list(node):
-    pass
+    seen = set()
+    curr = node
+    prev = None
+
+    while curr:
+        temp = curr.next
+
+        if curr.value in seen:
+            curr = temp
+            prev.next = curr
+        else:
+            seen.add(curr.value)    
+            prev = curr
+            curr = curr.next
+        
+    return node
+
+
+
+a = ListNode(3)
+b = ListNode(4)
+c = ListNode(3)
+d = ListNode(2)
+e = ListNode(6)
+f = ListNode(1)
+g = ListNode(2)
+h = ListNode(6)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+f.next = g
+g.next = h
+
+print(condense_linked_list(a)) # expected: (3) -> (4) -> (2) -> (6) -> (1) -> N
